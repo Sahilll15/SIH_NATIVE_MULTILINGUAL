89ec15d58a771130c5c8f5f8671d6fb3c9f44b88
Aditya <192105adityashah@gmail.com>
2023-12-20 09:03:21 +0530

    new pages

--- App.js
+++ App.js
@@ -49,6 +49,7 @@ import BailDetail from './screens/Court/BailDetail';
 import LawyerDetailsPage from './screens/Prisioner/LawyerSelect';
 import Rights from './screens/Prisioner/Rights';
 import Bot from './screens/Bot';
+import ProductUpload from './screens/Guard/ProductUpload';
 // import ChatBot from './screens/ChatBot/ChatBot';
 const Stack = createNativeStackNavigator();
 // new@gmail.com
@@ -62,7 +63,7 @@ export default function App() {
 
 
           <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator initialRouteName="Home">
               <Stack.Screen name='Land' component={Land} />
               <Stack.Screen name="Home" component={HomeScreen} />
               <Stack.Screen name="Rights" component={Rights} />
@@ -106,6 +107,7 @@ export default function App() {
               <Stack.Screen name="LawyerConnect" component={LawyerConnect} />
               <Stack.Screen name="BailList" component={BailList} />
               <Stack.Screen name="BailDetail" component={BailDetail} />
+              <Stack.Screen name="ProductUpload" component={ProductUpload} />
             </Stack.Navigator>
             <Bottom />
             <Toast ref={(ref) => Toast.setRef(ref)} />
